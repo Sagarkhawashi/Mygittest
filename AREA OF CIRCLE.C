ee574db029bd337ada70bea6bbdefb2e515dc8c4
Sagarkhawashi <100435445+Sagarkhawashi@users.noreply.github.com>
2022-03-02 19:27:12 +0530

    Add files via upload

---
+++ AREA OF CIRCLE.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+	int r,area;                                  //declaration
+	
+	r=2;                                           //initialization          
+	 
+	area=3.14*r*r;                                       //logic
+	printf("Area of circle=%d",area);            //output
+	
+	return 0;
+}
